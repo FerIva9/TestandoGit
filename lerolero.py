5b0a83f0b9672abe85f227cbd409447078ef16c0
Fernando Ivanov <fernandotadeuivanov@gmail.com>
2025-03-31 20:32:06 -0300

    Cria esqueleto em inglês.

--- lerolero.py
+++ lerolero.py
@@ -12,6 +12,14 @@ parte1 = []
 parte2 = []
 parte3 = []
 
+lingua = int(input("Escolha a lingua: 1 - portugues; 2 - ingles\n"))
+
+if lingua == 2:
+	parte1 = []
+	parte2 = []
+	parte3 = []
+
+
 # Combina as partes aleatoriamente
 print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
 
